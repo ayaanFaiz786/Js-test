b8c6d44da0fd4589cde9e7eac6f8ca0e84fb37b1
ayaanFaiz786 <70535597+ayaanFaiz786@users.noreply.github.com>
2024-10-19 05:02:21 +0000

    data type conversion

---
+++ 03_convertionOperation.js
@@ -0,0 +1,25 @@
+// conversion and operation
+
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let scoreInNumber = Number(score);
+// console.log(typeof scoreInNumber);
+
+// covertion possiblity
+// "33" => 33
+// "33abc" => NaN
+// true =>  1, fasle => 0 
+
+let isLoggedIn = "1";
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(booleanLoggedIn);
+
+//  1 => true, 0 = false;
+// "" => false, "test" => true
+
+let num = 22;
+let stringNum = String(num);
+
