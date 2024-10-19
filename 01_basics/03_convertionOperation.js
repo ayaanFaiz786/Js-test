df4e572edec19d1a184747b6463576a1b7fd8acf
ayaanFaiz786 <70535597+ayaanFaiz786@users.noreply.github.com>
2024-10-19 05:37:33 +0000

    Operations

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -23,3 +23,20 @@ console.log(booleanLoggedIn);
 let num = 22;
 let stringNum = String(num);
 
+// Operations
+// + - addition, - => sub, * =>  multiplication, 2**3 =>  2 riase to power of 3
+
+// incremental peration 
+let x = 3;
+// it will assign and than increment
+const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+// it will increment and than assign
+const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
