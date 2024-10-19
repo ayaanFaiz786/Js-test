5c3a1669741294287dfacf21fa41d7576ec9284b
ayaanFaiz786 <70535597+ayaanFaiz786@users.noreply.github.com>
2024-10-19 15:07:10 +0000

    strings

---
+++ 01_basics/06_string.js
@@ -0,0 +1,20 @@
+const name = 'ayaan';
+const age = '27';
+
+// old way
+// console.log(name + age);
+
+// new way using back ticks
+console.log(`My name is ${name} and age is ${age}`);
+
+// diff way of defining string
+const value = new String('Faizan');
+// console.log(value[0]);
+console.log(value.__proto__);
+console.log(value.length);
+console.log(value.toUpperCase());
+console.log(value.charAt(2));
+console.log(value.indexOf('z'));
+
+// can learn string based funtions 
+
