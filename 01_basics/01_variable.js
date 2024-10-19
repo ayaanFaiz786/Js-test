e5ecbf285f0b7f158716d33c9eec0535d0419a16
ayaanFaiz786 <70535597+ayaanFaiz786@users.noreply.github.com>
2024-10-19 04:46:03 +0000

    Variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+// variables
+const accountNumber = 123445555;
+let accountName = 'Albert';
+var accountEmail = 'abc@mail.com';
+accountCity = 'delhi';
+
+// accountNumber = 23826732 not allowed
+accountName = 'albert1';
+accountEmail = 'new@mail.com';
+accountCity = 'pune';
+
+// use let instead of var
+
+console.table([accountName, accountEmail, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";  // treat all js code as newer version 
+// alert('test'); this will not work here it works on browser not in node js
+
+// number => 2 to the power of 53
+// bigint
+// string
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+// object
+
+console.log(typeof "test"); // string
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
