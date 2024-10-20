5778963f00712cf52a60f2037ad7582ff10219ab
ayaanFaiz786 <70535597+ayaanFaiz786@users.noreply.github.com>
2024-10-20 06:37:43 +0000

    dates

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,33 @@
+// dates
+
+let myDate = new Date();
+
+// console.log(myDate);
+// 2024-10-20T06:17:01.560Z
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+// date is a object
+
+// create custom date
+// first param is year, month, day (Month in js starts with 0)
+let myCreatedDate = new Date(2023, 0, 22);
+// let myCreatedDateWithTIme = new Date(2023, 0, 22, 5, 20);
+// let newDate = new Date('12-01-2023');
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDateWithTIme.toLocaleString());
+// console.log(newDate.toLocaleString());
+
+// timestamp
+// let myTimeStamp = Date.now();
+// get timestamp from date
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+
+
+
+
+
